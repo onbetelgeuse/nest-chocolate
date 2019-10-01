7d9bb6873679a8376f295b30437a974f65276f51
Jean-Michel FRECINAT <onbetelgeuse@gmail.com>
2019-10-01 11:20:45 +0200

    fix delete findone

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -6,12 +6,19 @@ import {
   UploadedFiles,
   UseInterceptors,
   UseGuards,
+  Delete,
+  Param,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { AuthGuard } from '@nestjs/passport';
 import { FileDto } from './dto/file.dto';
 import { User } from '../common/decorators/user.decorator';
+import { Roles } from '../common/decorators/roles.decorator';
+import { RolesGuard } from '../common/guards/roles.guard';
+import * as fs from 'fs';
 
 @Controller('files')
 export class FilesController {
@@ -20,7 +27,7 @@ export class FilesController {
 
   @Post('/upload')
   @UseGuards(AuthGuard())
-  @UseInterceptors(FilesInterceptor('file', 200))
+  @UseInterceptors(FilesInterceptor('file', 20))
   public async uploadFile(
     @UploadedFiles() files: FileDto[],
     @User('id') userId: number,
@@ -33,5 +40,38 @@ export class FilesController {
   public async findAll(@User('id') id: number): Promise<FileDto[]> {
     const results = await this.filesService.findAllByUserId(id);
     return results.map(FileDto.fromEntity);
+  }
+
+  @Get(':filename')
+  @UseGuards(AuthGuard())
+  public async findOne(
+    @User('id') id: number,
+    @Param('filename') filename: string,
+  ): Promise<FileDto> {
+    try {
+      const result = await this.filesService.findOneByUserId(id, filename);
+      return FileDto.fromEntity(result);
+    } catch (error) {
+      this.logger.log(error.message);
+      throw new HttpException('File is not found.', HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @Delete(':filename')
+  @Roles('admin')
+  @UseGuards(AuthGuard(), RolesGuard)
+  public async delete(
+    @User('id') id: number,
+    @Param('filename') filename: string,
+  ): Promise<boolean> {
+    try {
+      const file = await this.filesService.findOneByUserId(id, filename);
+      fs.unlinkSync(file.path);
+      const result = await this.filesService.deleteOneByUserId(id, filename);
+      return result.affected > 0;
+    } catch (error) {
+      this.logger.log(error.message);
+      throw new HttpException('File is not found.', HttpStatus.NOT_FOUND);
+    }
   }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -3,6 +3,7 @@ import { FileRepository } from './file.repository';
 import { File } from './file.entity';
 import { User } from '../user/user.entity';
 import { FileDto } from './dto/file.dto';
+import { DeleteResult } from 'typeorm';
 
 @Injectable()
 export class FilesService {
@@ -13,20 +14,27 @@ export class FilesService {
     return this.fileRepository.findAllByUserId(userId);
   }
 
+  public async findOneByUserId(
+    userId: number,
+    filename: string,
+  ): Promise<File> {
+    return this.fileRepository.findOneByUserId(userId, filename);
+  }
+
+  public async deleteOneByUserId(
+    userId: number,
+    filename: string,
+  ): Promise<DeleteResult> {
+    return this.fileRepository.deleteOneByUserId(userId, filename);
+  }
+
   public async saveAllByUserId(
     userId: number,
     files: FileDto[],
   ): Promise<File[]> {
     const entities: File[] = files.map(file => {
       const entity = new File();
-      entity.filename = file.filename;
-      entity.destination = file.destination;
-      entity.encoding = file.encoding;
-      entity.fieldname = file.fieldname;
-      entity.mimetype = file.mimetype;
-      entity.originalname = file.originalname;
-      entity.path = file.path;
-      entity.size = file.size;
+      Object.assign(entity, file);
       entity.user = new User();
       entity.user.id = userId;
       return entity;
