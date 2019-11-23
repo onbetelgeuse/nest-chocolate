19f6b2f78a636e608d397ed57e55f7c2cb7fbeec
Jean-Michel FRECINAT <onbetelgeuse@gmail.com>
2019-11-23 20:23:16 +0100

    fix test

--- src/files/files.controller.spec.ts
+++ src/files/files.controller.spec.ts
@@ -1,15 +1,21 @@
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
 import { mock, instance } from 'ts-mockito';
+import { ConfigService } from '../config/config.service';
 
 describe('File Controller', () => {
   let controller: FilesController;
   let fileService: FilesService;
+  let configService: ConfigService;
 
   beforeEach(async () => {
     fileService = mock(FilesService);
+    configService = mock(ConfigService);
 
-    controller = new FilesController(instance(fileService));
+    controller = new FilesController(
+      instance(fileService),
+      instance(configService),
+    );
   });
 
   it('should be defined', () => {
